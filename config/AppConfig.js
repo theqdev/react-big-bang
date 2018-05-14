399d73bd94e9febe4b7a9628d068f57eef5e2cf9
George Damian <ifkinlovemetro@hotmail.com>
2018-05-15 02:08:53 +0300

    Config updates

--- config/AppConfig.js
+++
@@ -1,7 +0,0 @@
-export const settings = {
-  appName : 'Private Notes',
-  appSlogan : 'Send private notes that self destruct after being read',
-  appDomain: 'https://privatenotes.qwebdev.net',
-  appDomainlabel: 'PrivateNotes.cc',
-  APILink: 'https://pn.q-dev.co/api/v1'
-};
---
+++ src/Config/AppConfig.js
@@ -0,0 +1,10 @@
+// Simple React Native specific changes
+
+export default {
+  // font scaling override - RN default is on
+  allowTextFontScaling: true,
+  AppName: 'Binder',
+  APIServer: 'http://localhost:8000/api/v1',
+  AdMobBannerID: '',
+  FCMKey: '',
+}
