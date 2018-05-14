cc6cef5296f696cc8291346ec293313dfc8074c2
George Damian <ifkinlovemetro@hotmail.com>
2018-05-15 02:03:08 +0300

    More boilerplate cleanings

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,6 +1,6 @@
 import {example as example} from './Example';
 import { combineReducers } from 'redux'
-import configureStore from '../store/CreateStore'
+import configureStore from '../Store/CreateStore'
 import rootSaga from '../Sagas/index'
 
 export default () => {
@@ -8,16 +8,6 @@ export default () => {
   const rootReducer = combineReducers({
 
     example:example,
-    // app: require('./UserRedux').reducer,
-    // users: require('./UsersRedux').reducer,
-    // settings: require('./UserSettings').reducer,
-    // profile: require('./UserProfile').reducer,
-    // matches: require('./MatchesRedux').reducer,
-    // location: require('./LocationRedux').reducer,
-    // conversations: require('./UserMessages').reducer,
-    // messages: require('./ChatRoomRedux').reducer,
-    // report: require('./UserReportRedux').reducer,
-    // notification: require('./NotificationRedux').reducer
   })
 
   return configureStore(rootReducer, rootSaga)
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Switch, NavLink, Route, Link } from 'react-router-dom';
-// import HomePage from './HomePage';
-// import FuelSavingsPage from './Containers/FuelSavingsPage';
-// import AboutPage from './AboutPage';
-// import NotFoundPage from './NotFoundPage';
-// import Home from '../Containers/Home';
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header,  Container  } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import { Container  } from 'semantic-ui-react'
 import AppNavigation from "../Navigation/Routes";
-// import { Link } from 'react-router'
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
 
 class App extends Component {
 
@@ -29,7 +20,7 @@ class App extends Component {
           <Container>
 
           <div className="header item">
-            PUBG Tracker
+            React JS BoilerPlate
           </div>
 
             <Link className={this.props.history.location.pathname == '/' ? "item active" : "item"} to="/">Home</Link>
